0b31afa393476406fda79bafe1248a70b64a8c4a
Travelle Whitney <travellewhitney@gmail.com>
2024-08-01 17:08:58 -0500

    completed

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(Ralph) {
+    cats.push(Ralph);
+  }
+
+  function destructivelyPrependCat(Bob) {
+    cats.unshift(Bob);
+  }
+
+  function destructivelyRemoveLastCat(Garfield) {
+    cats.pop(Garfield);
+  }
+
+  function destructivelyRemoveFirstCat(Milo) {
+    cats.shift(Milo);
+  }
+
+  function appendCat(Broom) {
+    return cats.concat(Broom);
+  }
+
+  function prependCat(Arnold) {
+    return [Arnold, ...cats];
+  }
+
+  function removeLastCat(Garfield) {
+    return cats.slice(0, -1);
+  }
+  
+  function removeFirstCat(Milo) {
+    return cats.slice(1);
+  }
